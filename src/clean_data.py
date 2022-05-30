e974f6bd02f6d87137eca6397e280d1a845e47d6
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-05-30 17:29:57 -0500

    Create clean_data.py

---
+++ src/clean_data.py
@@ -0,0 +1,24 @@
+import logging
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def clean_data(data: pd.DataFrame, duplicated_method: str = "first") -> pd.DataFrame:
+
+    data_before_shape = data.shape
+    logger.info("%d records and %d columns in raw data.", data_before_shape[0], data_before_shape[1])
+    count_duplicate_rows = data.duplicated(keep=duplicated_method).sum()
+    data = data.drop_duplicates(keep=duplicated_method)
+    count_null_values = data.isnull().sum()
+    data = data.dropna()
+    data_after_shape = data.shape
+
+    logger.info("Dropped %d duplicate rows and %d rows with NA values.",
+                count_duplicate_rows.sum(),
+                count_null_values.sum())
+    logger.info("Clean data has %d records and %d columns.", data_after_shape[0], data_after_shape[1])
+    data = data.reset_index(drop=True)
+
+    return data
