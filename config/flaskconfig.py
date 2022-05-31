a7149729264ab121773874d5dcb62f3b61e9a424
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-05-30 23:57:49 -0500

    Add model config path

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,4 +1,5 @@
 import os
+
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 5000
@@ -9,11 +10,12 @@ SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 10
 ROW_SORT_BY = "query_count"
 
-PATH_TRAINED_MODEL_OBJECT = './models/trained_model_object1.joblib'
+PATH_TRAINED_MODEL_OBJECT = './models/trained_model_object.joblib'
 PATH_TRAINED_ONE_HOT_ENCODER = './models/ohe_object.joblib'
+MODEL_CONFIG_PATH = "./config/model_config.yaml"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  # This must be an env var which exists in docker
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 if SQLALCHEMY_DATABASE_URI is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/richard.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/traffic_prediction.db'
 
 
