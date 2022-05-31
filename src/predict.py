818e677328c0708f83738004cf529ac4ee802951
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-05-30 21:42:44 -0500

    Implement predict function

--- src/predict.py
+++ src/predict.py
@@ -1,14 +1,12 @@
 import logging
 
 import pandas as pd
-import sklearn.linear_model
 import sklearn.preprocessing
-from sklearn.ensemble import RandomForestRegressor
 import sklearn.model_selection
-import yaml
-import joblib
+import sklearn.base
+import sklearn.exceptions
 
-import src.s3_actions
+import src.read_write_s3
 import src.data_preprocessing
 
 logger = logging.getLogger(__name__)
@@ -44,27 +42,78 @@ def predict_preprocess(predictors: dict, preprocess_params: dict) -> pd.DataFram
     return prediction_df
 
 
-def predict(new_data: pd.DataFrame, model_object_path: str = "", ohe_object_path: str = "", s3_bool: bool = False):
-    one_hot_encode_columns = ["weather_main", "month", "hour", "day_of_week"] # TODO USE YAML
-    if s3_bool:
-        src.s3_actions.s3_read_from_file(model_object_path, "./trained_model_object_s3.joblib") #TODO Fix hardcoding
-        src.s3_actions.s3_read_from_file(ohe_object_path, "./ohe_object.joblib")
+def ohe_new_predict():
+    pass
+    # one_hot_encode_columns = ["weather_main", "month", "hour", "day_of_week"] # TODO USE YAML
+    # # if s3_bool:
+    # #     src.s3_actions.s3_read_from_file(model_object_path, "./trained_model_object_s3.joblib") #TODO Fix hardcoding
+    # #     src.s3_actions.s3_read_from_file(ohe_object_path, "./ohe_object.joblib")
+    # logger.info("Attempt to load model objects")
+    # try:
+    #     model_object = joblib.load(model_object_path)
+    # except Exception as e:
+    #     logger.error(e)
+    # logger.info("Loaded Trained Model Object")
+    # try:
+    #     one_hot_encoder = joblib.load(ohe_object_path)
+    # except Exception as e:
+    #     logger.error(e)
+    # logger.info("Loaded One Hot Encoder")
+    #
+    # #new_data = pd.DataFrame(predictors)
+    # one_hot_array = one_hot_encoder.transform(new_data[["weather_main", "month", "hour", "day_of_week"]]) # TODO use yaml config herer
+    # #print(ohe_new_data)
+    #
+    # one_hot_column_names = one_hot_encoder.get_feature_names_out()
+    # one_hot_df = pd.DataFrame(one_hot_array, columns=one_hot_column_names)
+    # data_one_hot_encoded = new_data.join(one_hot_df).drop(one_hot_encode_columns, axis=1)
+    # print(data_one_hot_encoded.head())
+    # logger.info("One Hot Encoded the new data")
 
-    model_object = joblib.load("./models/trained_model_object1.joblib")
-    one_hot_encoder = joblib.load("./models/ohe_object.joblib")
 
-    #new_data = pd.DataFrame(predictors)
-    one_hot_array = one_hot_encoder.transform(new_data[["weather_main", "month", "hour", "day_of_week"]]) # TODO use yaml config herer
-    #print(ohe_new_data)
+def make_predictions(new_data: pd.DataFrame, model: sklearn.base.BaseEstimator, response_column: str) -> pd.Series:
 
-    one_hot_column_names = one_hot_encoder.get_feature_names_out()
-    one_hot_df = pd.DataFrame(one_hot_array, columns=one_hot_column_names)
-    data_one_hot_encoded = new_data.join(one_hot_df).drop(one_hot_encode_columns, axis=1)
-    print(data_one_hot_encoded.head())
-    prediction = model_object.predict(data_one_hot_encoded)
-    print(prediction)
-    return prediction
+    predictions_series = pd.Series(dtype='float64')
+    predictions = None
+    try:
+        predictors = new_data.drop([response_column], axis=1)
+    except KeyError as key_error:
+        logger.error("The response column was not present in the dataframe. Returning an empty series.", key_error)
+        return predictions_series
 
+    successful_prediction = True
+
+    try:
+        predictions = model.predict(predictors)
+    except sklearn.exceptions.NotFittedError as model_not_fitted:
+        # This error will occur if the sklearn model object was not successfully fit in model training.
+        logger.error("Prediction failed. Attempting to predict using an unfit model. %s", model_not_fitted)
+        successful_prediction = False
+    except KeyError as key_error:
+        # This error can occur if the predictor columns are not in the test set.
+        logger.error("Prediction failed. The required columns were not present in the dataset. %s", key_error)
+        successful_prediction = False
+    except ValueError as val_error:
+        # This error can occur if the model object has invalid hyperparameters.
+        logger.error("Prediction failed. The model object contained invalid hyperparameters. %s", val_error)
+        successful_prediction = False
+    except TypeError as type_error:
+        # This error can occur if the model object has hyperparameters of an invalid data type.
+        logger.error("Prediction failed. The model object contained hyperparameters with an invalid datatype."
+                     " %s", type_error)
+        successful_prediction = False
+
+    # If the prediction is successful, convert the results to pandas Series and return them
+    if successful_prediction:
+        predictions_series = pd.Series(predictions)
+
+        logger.info("Successfully predicted estimates.")
+
+    # If prediction fails, print a warning.
+    else:
+        logger.warning("Prediction failed. No output files saved. Returning empty an dataframe.")
+
+    return predictions_series
 
 # with open("/Users/richard/Documents/school_work/SpringQuarter/AVC/2022-msia423-hathaway-richard-project/config/model_config.yaml", "r", encoding="utf-8") as preprocess_yaml:
 #     preprocess_parameters = yaml.load(preprocess_yaml, Loader=yaml.FullLoader)
