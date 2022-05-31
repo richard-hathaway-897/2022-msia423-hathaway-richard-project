ce4295827375352817da05e850faa4d6ab0b07f3
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-05-30 21:42:20 -0500

    Add data validation and remove un-needed code

--- src/orchestrate.py
+++ src/orchestrate.py
@@ -12,8 +12,9 @@ import src.train_model
 import src.read_write_functions
 import src.validate
 import src.read_write_s3
+import src.predict
+import src.evaluate_model
 
-logging.config.fileConfig("config/logging/local.conf")
 logger = logging.getLogger(__name__)
 
 
@@ -51,9 +52,12 @@ def run_clean_data(command_line_args: argparse.Namespace, config_dict: dict) -> 
     except ValueError:
         logger.error("Failed to read raw data from S3 for data cleaning.")
     else:
-        # TODO: Add data validation
-        cleaned_data = src.clean_data.clean_data(data=raw_data, **config_dict["clean_data"])
-        src.read_write_functions.save_csv(cleaned_data, command_line_args.output_path)
+        valid_data = src.validate.validate_dataframe(
+                     raw_data,
+                     **config_dict["validate_dataframe"])
+        if valid_data:
+            cleaned_data = src.clean_data.clean_data(data=raw_data, **config_dict["clean_data"])
+            src.read_write_functions.save_csv(cleaned_data, command_line_args.output_path)
 
 def run_generate_features(command_line_args: argparse.Namespace, config_dict: dict) -> None:
     """This function calls the necessary functions to load data, generate features, and save the train/test data.
@@ -67,125 +71,91 @@ def run_generate_features(command_line_args: argparse.Namespace, config_dict: di
     """
     # Read the input cleaned data
     input_data = src.read_write_functions.read_csv(command_line_args.input_source)
+    valid_data = src.validate.validate_dataframe(
+        input_data,
+        **config_dict["validate_dataframe"])
 
-    train_data, test_data, one_hot_encoder = \
-        src.data_preprocessing.generate_features(data=input_data,
-                                                 remove_outlier_params=config_dict["remove_outliers"],
-                                                 **config_dict["generate_features"])
+    if valid_data:
+        train_data, test_data, one_hot_encoder = \
+            src.data_preprocessing.generate_features(data=input_data,
+                                                     remove_outlier_params=config_dict["remove_outliers"],
+                                                     **config_dict["generate_features"])
+        train_data = src.clean_data.clean_data(data=train_data, **config_dict["clean_data"])
+        test_data = src.clean_data.clean_data(data=test_data, **config_dict["clean_data"])
 
-    # Save the train and test data, and print out messages if successful.
-    src.read_write_functions.save_csv(train_data, command_line_args.train_output_source)
-    src.read_write_functions.save_csv(test_data, command_line_args.test_output_source)
+        # Save the train and test data, and print out messages if successful.
+        src.read_write_functions.save_csv(train_data, command_line_args.train_output_source)
+        src.read_write_functions.save_csv(test_data, command_line_args.test_output_source)
 
-# def run_train_model(command_line_args: argparse.Namespace, config_dict: dict) -> None:
-#     """This function calls the necessary functions to load train data, fit the model object, and save the model_object.
+def run_train_model(command_line_args: argparse.Namespace, config_dict: dict) -> None:
+    """This function calls the necessary functions to load train data, fit the model object, and save the model_object.
+
+    Args:
+        command_line_args (argparse.Namespace): The command line arguments passed to the program.
+        config_dict (dict): A dictionary of configuration parameters
+
+    Returns:
+        This function does not return any object.
+    """
+    # Read in the training data
+    training_data = src.read_write_functions.read_csv(command_line_args.train_input_source)
+    valid_data = src.validate.validate_dataframe(
+        training_data,
+        **config_dict["validate_dataframe"])
+    if valid_data:
+        try:
+            trained_model = src.train_model.train_model(train_data=training_data,
+                                                        **config_dict["model_training"]["random_forest"])
+        except KeyError:
+            logger.error("The response column was not found in the training data.")
+        else:
+            src.read_write_functions.save_model_object(trained_model, command_line_args.model_output_source)
+
+
+def run_predict(command_line_args: argparse.Namespace, config_dict: dict) -> None:
+    """This function calls the necessary functions to load the test data and trained model object,
+        make predictions using the model, and save the model predictions.
+
+    Args:
+        command_line_args (argparse.Namespace): The command line arguments passed to the program.
+        config_dict (dict): A dictionary of configuration parameters
+
+    Returns:
+        This function does not return any object.
+    """
+    # Read in the test data
+    test_data = src.read_write_functions.read_csv(command_line_args.test_input_source)
+    model = src.read_write_functions.load_model_object(command_line_args.model_input_source)
+    valid_data = src.validate.validate_dataframe(
+        test_data,
+        **config_dict["validate_dataframe"])
+    if valid_data and model is not None:
+        predictions = src.predict.make_predictions(new_data=test_data, model=model, **config_dict["predict"])
+        if not predictions.empty:
+            src.read_write_functions.save_csv(predictions,
+                                              command_line_args.predictions_output_source)
 #
-#     Args:
-#         command_line_args (argparse.Namespace): The command line arguments passed to the program.
-#         config_dict (dict): A dictionary of configuration parameters
-#
-#     Returns:
-#         This function does not return any object.
-#     """
-#     # Read in the training data
-#     input_data = src.helper_functions.read_csv(command_line_args.train_input_source)
-#
-#     # Perform data validation
-#     all_columns = config_dict["columns"]["predictor_columns"] + [config_dict["columns"]["target_column_name"]]
-#     valid_data = src.validate.validate_clouds(data=input_data,
-#                                               expected_columns=all_columns, **config_dict["validate_clouds"])
-#     # If the data is valid, train the model
-#     if valid_data:
-#         trained_model = src.train_model.train_model(train_data=input_data,
-#                                                     **config_dict["train_model"]["model_columns"],
-#                                                     **config_dict["train_model"]["random_forest_classifier"])
-#         src.helper_functions.save_model_object(trained_model, command_line_args.model_output_source)
-#
-#
-# def run_score_model(command_line_args: argparse.Namespace, config_dict: dict) -> None:
-#     """This function calls the necessary functions to load the test data and trained model object,
-#         make predictions using the model, and save the model predictions.
-#
-#     Args:
-#         command_line_args (argparse.Namespace): The command line arguments passed to the program.
-#         config_dict (dict): A dictionary of configuration parameters
-#
-#     Returns:
-#         This function does not return any object.
-#     """
-#     # Read in the test data
-#     test_data = src.helper_functions.read_csv(command_line_args.test_input_source)
-#
-#     # Perform data validation
-#     all_columns = config_dict["columns"]["predictor_columns"] + [config_dict["columns"]["target_column_name"]]
-#     valid_data = src.validate.validate_clouds(data=test_data,
-#                                               expected_columns=all_columns, **config_dict["validate_clouds"])
-#
-#     # Load the trained model object
-#     model = src.helper_functions.load_model_object(command_line_args.model_input_source)
-#
-#     # If loading data was successful, call the score_model function
-#     if valid_data and (model is not None):
-#         class_predictions, probability_predictions = src.score_evaluate_model.score_model(model=model,
-#                                                                                           test_data=test_data,
-#                                                                                           **config_dict["score_model"])
-#         # If the score_model function was successful, then save the output files.
-#         # Check if the output files from the previous step are empty
-#         if (not class_predictions.empty) and (not probability_predictions.empty):
-#             class_status = src.helper_functions.save_csv(class_predictions,
-#                                                          command_line_args.class_output_source)
-#             prob_status = src.helper_functions.save_csv(probability_predictions,
-#                                                         command_line_args.prob_output_source)
-#             # If saving was successful, log out a message.
-#             if class_status and prob_status:
-#                 logger.info("Successfully saved class predictions and probability predictions to '%s' and '%s'",
-#                             command_line_args.class_output_source,
-#                             command_line_args.prob_output_source)
-#             else:
-#                 logger.error("Failed to save class predictions and probability predictions.")
-#
-#
-# def run_evaluate_model(command_line_args: argparse.Namespace, config_dict: dict) -> None:
-#     """This function calls the necessary functions to load the test data and model predictions,
-#         create model evaluation metrics, and save the model evaluation metrics.
-#
-#     Args:
-#         command_line_args (argparse.Namespace): The command line arguments passed to the program.
-#         config_dict (dict): A dictionary of configuration parameters
-#
-#     Returns:
-#         This function does not return any object.
-#     """
-#
-#     # Read in the test data
-#     test_data = src.helper_functions.read_csv(command_line_args.test_input_source)
-#     all_columns = config_dict["columns"]["predictor_columns"] + [config_dict["columns"]["target_column_name"]]
-#
-#     # Perform data validation
-#     valid_data = src.validate.validate_clouds(data=test_data,
-#                                               expected_columns=all_columns, **config_dict["validate_clouds"])
-#     # Read in the predicted values
-#     class_predictions = src.helper_functions.read_csv(command_line_args.class_input_source)
-#     prob_predictions = src.helper_functions.read_csv(command_line_args.prob_input_source)
-#
-#     # If the data is valid, call the evaluate_model function
-#     if valid_data and (not class_predictions.empty) and (not prob_predictions.empty):
-#         metrics, confusion_matrix, class_report = \
-#             src.score_evaluate_model.evaluate_model(test=test_data,
-#                                                     ypred_proba_test=prob_predictions,
-#                                                     ypred_bin_test=class_predictions,
-#                                                     response_column=config_dict["columns"]["target_column_name"],
-#                                                     **config_dict["evaluate_model"]["confusion_matrix"])
-#
-#         # Save the output files from evaluate_model
-#         metrics_save_status = src.helper_functions.save_csv(metrics, command_line_args.auc_accuracy_output_source)
-#         confusion_matrix_save_status = src.helper_functions.save_csv(confusion_matrix,
-#                                                                      command_line_args.auc_accuracy_output_source)
-#         class_save_status = src.helper_functions.save_csv(class_report, command_line_args.report_output_source)
-#
-#         # If saving was successful, log out a message.
-#         if metrics_save_status and confusion_matrix_save_status and class_save_status:
-#             logger.info("Successfully saved model evaluation artifacts to '%s', '%s', '%s'.",
-#                         command_line_args.auc_accuracy_output_source,
-#                         command_line_args.auc_accuracy_output_source,
-#                         command_line_args.report_output_source)
+def run_evaluate(command_line_args: argparse.Namespace, config_dict: dict) -> None:
+    """This function calls the necessary functions to load the test data and model predictions,
+        create model evaluation metrics, and save the model evaluation metrics.
+
+    Args:
+        command_line_args (argparse.Namespace): The command line arguments passed to the program.
+        config_dict (dict): A dictionary of configuration parameters
+
+    Returns:
+        This function does not return any object.
+    """
+
+    # Read in the test data
+    test_data = src.read_write_functions.read_csv(command_line_args.test_input_source)
+    valid_data = src.validate.validate_dataframe(
+        test_data,
+        **config_dict["validate_dataframe"])
+    predictions = src.read_write_functions.read_csv(command_line_args.predictions_input_source)
+    if valid_data and not predictions.empty:
+        metrics_dict = src.evaluate_model.evaluate_model(test=test_data,
+                                                         predictions=predictions,
+                                                         **config_dict["predict"])
+        src.read_write_functions.save_dict_as_text(data=metrics_dict,
+                                                   output_path=command_line_args.performance_metrics_output_source)
