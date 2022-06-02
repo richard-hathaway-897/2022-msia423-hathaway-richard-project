7d63c927ef18deb2eafd2d9f392faa3dc5a4856c
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-06-02 14:19:30 -0500

    Remove unneccessary code and add a log message

--- src/train_model.py
+++ src/train_model.py
@@ -16,11 +16,10 @@ def train_model(train_data: pd.DataFrame,
                 n_jobs,
                 random_state):
 
-    # TODO: Data Validation when reading in. CHECK FOR NULLS!
     try:
         predictors = train_data.drop([response_column], axis=1)
     except KeyError as key_error:
-        logger.error(key_error)
+        logger.error("The specified response column does not exist in the training data. %s", key_error)
         raise key_error
 
     response = train_data[response_column]
@@ -39,16 +38,6 @@ def train_model(train_data: pd.DataFrame,
         logger.info("Random Forest training OOB Score was: %f", rf_model.oob_score_)
 
     return rf_model
-
-        # test_r2 = rf_model.score(X = test_predictors, y = test_response)
-        # logger.info("Random Forest R^2 on validation set: %f", test_r2)
-        #
-        # # TODO Add option to not upload to S3 and just save locally.
-        # joblib.dump(rf_model, model_output_local_path)
-        #
-        # if model_output_s3_path is not "None":
-        #     src.s3_actions.s3_write_from_file(model_output_local_path, model_output_s3_path)
-
 
 
 
