a7544ebe64a4c6bc361475c984a22236d7d5fa8e
Richard Hathaway <richardhathaway2022@u.northwestern.edu>
2022-06-04 13:34:11 -0500

    Create database_config.py

---
+++ config/database_config.py
@@ -0,0 +1,15 @@
+"""
+This file contains configuration values used to create and update tables in create_tables_rds.py
+"""
+
+# Values for creating/updating rows in HistoricalQueries
+INITIAL_QUERY_COUNT = 1
+INCREMENT_VALUE = 1
+
+# Values to initialize the row in AppMetrics table
+INITIAL_LIKES = 0
+INITIAL_DISLIKES = 0
+
+# Values to initialize the row in AppPredictions table.
+INITIAL_PREDICTION = 0
+INITIAL_VOLUME = "light"
